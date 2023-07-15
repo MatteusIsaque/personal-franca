c4c7942d399fa315c544a06f1b79090d26dfabc1
Matteus Isaque <matteus.isaque28@gmail.com>
2023-07-15 10:35:03 -0300

    alt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-      <script
+      {/* <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -27,7 +27,7 @@ export default function RootLayout({
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-MDSPGZ6');`,
           }}
-        />
+        /> */}
       </head>
       <body className={inter.className}>
       <Nav />
