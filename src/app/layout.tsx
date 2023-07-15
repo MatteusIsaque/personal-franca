9861c510b17bc435bcadc3d424c5773624a750e6
Matteus Isaque <matteus.isaque28@gmail.com>
2023-07-15 10:38:16 -0300

    alt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,48 @@
-import Nav from '@/components/nav'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/footer'
-import { Metadata } from 'next'
+import Nav from "@/components/nav";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer";
+import { Metadata } from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "França Personal Trainer",
-  description: "França personal trainer, tenha resultados rápidos para seus treinos."
-}
+  description:
+    "França personal trainer, tenha resultados rápidos para seus treinos.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
       <head>
-      {/* <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MDSPGZ6');`,
+            })(window,document,'script','dataLayer','GTM-MDSPGZ6')`,
           }}
-        /> */}
+        />
       </head>
       <body className={inter.className}>
-      <Nav />
-      {children}
-      <Footer />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MDSPGZ6"
+            height="0"
+            width="0"
+            className="none hidden"
+          ></iframe>
+        </noscript>
+        <Nav />
+        {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
